cf98bf05e76c1241bea77a75fc52b5ce73a221c4
svlandeg <sofie.vanlandeghem@gmail.com>
2020-10-01 13:47:19 +0200

    check whether there are any listeners

--- spacy_transformers/pipeline_component.py
+++ spacy_transformers/pipeline_component.py
@@ -276,7 +276,8 @@ class Transformer(Pipe):
         batch_id = TransformerListener.get_batch_id(docs)
         for listener in self.listeners[:-1]:
             listener.receive(batch_id, trf_full.doc_data, accumulate_gradient)
-        self.listeners[-1].receive(batch_id, trf_full.doc_data, backprop)
+        if self.listeners[:-1]:
+            self.listeners[-1].receive(batch_id, trf_full.doc_data, backprop)
         if set_annotations:
             self.set_annotations(docs, trf_full)
         return losses
